2738024118a53a34087d035b5302c7f739f4633d
sarah <ssh2198@columbia.edu>
2025-05-07 17:50:48 -0400

    clean wipe

--- app/ui/tabs/LibraryTab.py
+++
@@ -1,6 +0,0 @@
-from PyQt6.QtWidgets import QWidget
-
-class LibraryTab(QWidget):
-    def __init__(self):
-        super().__init__()
-        pass
--- app/ui/tabs/RecordTab2.py
+++
@@ -1,6 +0,0 @@
-from PyQt6.QtWidgets import QWidget
-
-class RecordTab(QWidget):
-    def __init__(self):
-        super().__init__()
-        pass
